407ceb75ba0e9682c5fddaea321d424f467e44b6
Sebastien Feser <sedukafe@gmail.com>
2021-02-21 10:27:09 +0100

    moon finished

--- Assets/Scripts/Moon.cs
+++ Assets/Scripts/Moon.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Moon : MonoBehaviour
@@ -14,13 +15,23 @@ public class Moon : MonoBehaviour
 
     private List<Rigidbody2D> moonRigidbody2Ds = new List<Rigidbody2D>();
     private List<Collider2D> moonColliders = new List<Collider2D>();
+    private List<SpriteRenderer> moonSpriteRenderers = new List<SpriteRenderer>();
+
+
+    [SerializeField] private float layerFadeOut = 0.01f;
+    [SerializeField] private SpriteRenderer fadeOutSpriteRenderer;
 
     private bool destroyed = false;
+
+    [SerializeField] private float timeBeforeMoonExplodes = 5f;
+
+    private float timer;
 
     private void Start()
     {
         moonRigidbody2Ds.AddRange(moonSprites.GetComponentsInChildren<Rigidbody2D>());
         moonColliders.AddRange(moonSprites.GetComponentsInChildren<Collider2D>());
+        moonSpriteRenderers.AddRange(moonSprites.GetComponentsInChildren<SpriteRenderer>());
     }
     private void Update()
     {
@@ -30,6 +41,20 @@ public class Moon : MonoBehaviour
                 cameraTransform.position.z);
             transform.position = new Vector2(cameraTransform.position.x + cameraPosition.x,
                 cameraTransform.position.y + cameraPosition.y);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (destroyed)
+        {
+            FadeOut();
+            timer += Time.deltaTime;
+        }
+
+        if (timer > timeBeforeMoonExplodes)
+        {
+            FadeOutMoon();
         }
     }
     private void OnTriggerEnter2D(Collider2D collider)
@@ -49,12 +74,34 @@ public class Moon : MonoBehaviour
                     colliders.enabled = true;
                 }
 
+                timer = 0;
+
                 destroyed = true;
                 //moonSpriteRenderer.SetActive(false);
             }
         }
 
 
+    }
+
+
+    void FadeOutMoon()
+    {
+        foreach (SpriteRenderer sprites in moonSpriteRenderers)
+        {
+            sprites.color -= new Color(0,0,0,layerFadeOut);
+
+            if (sprites.color.a <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
+    void FadeOut()
+    {
+        fadeOutSpriteRenderer.color += new Color(0,0,0,layerFadeOut);
     }
 
 }
